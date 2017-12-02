494df13341174da3c74890694975edcd03cfe7c5
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2017-12-02 14:54:33 +0600

    Defining Function Outside Class

--- defining_function_outside_class.cpp
+++ defining_function_outside_class.cpp
@@ -1,11 +1,9 @@
 /*
-
   1. We can define a function/property outside class. In this case we only declare the function inside the class.
 
   2. To define a function outside class we use this syntax: DataType ClassName :: functionName(){}
 
   3. We can't define a regular variable/property outside the class in the same way we do for function
-
 */
 
 #include <iostream>
